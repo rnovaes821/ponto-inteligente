3dcb789c33eb37307d0421b5d215bb87f04294e2
rnovaes821 <61747085+rnovaes821@users.noreply.github.com>
2020-03-03 14:59:36 -0300

    Add files via upload

---
+++ PontoInteligente/src/main/java/br/com/pontointeligente/api/PontoInteligenteApplication.java
@@ -0,0 +1,13 @@
+package br.com.pontointeligente.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontoInteligenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontoInteligenteApplication.class, args);
+	}
+
+}
---
+++ PontoInteligente/src/test/java/br/com/pontointeligente/api/PontoInteligenteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pontointeligente.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontoInteligenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
